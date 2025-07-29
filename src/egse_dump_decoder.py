69fef247c3f543808f5e26a3a034d9d44735b812
Alun Jones <alj118@aber.ac.uk>
2025-07-29 08:46:12 +0000

    Matt requested a change to CSV output
    Conversion of science data to CSV should match previous output generated by
    Dave's LabView scripts. I've added a "default_fields_per_type" to the
    EGSEDumpDecoder class. It's not used directly by the class, but decoders
    can use this as a field list handed to the csv() and csvheader() methods
    of the underlying classes.
    
    The __main__ section of egse_dump_decoder.py has an example of how this
    might be used.

--- src/egse_dump_decoder.py
+++ src/egse_dump_decoder.py
@@ -17,6 +17,18 @@ class EGSEDumpDecoder:
     struct_names = None
     struct_expected_length = None
 
+    # This list isn't used directly in the class, but we need
+    # somewhere sensible to store a default field list for decodes
+    # and here seems as good as anywhere.
+    default_fields_per_type = {
+        tm.SCI: [
+            "MOD_ID", "CMD_CNT", "ERROR_BYTE", "MTR_ABS_STEPS", "THRM_STATUS",
+            "SWIR_OFFSET", "MWIR_OFFSET", "SCI_ADC_SAMPLES", "SCI_ADC_SKIP",
+            "SWIR_HIGH", "SWIR_MED", "SWIR_LOW", "MWIR_HIGH", "MWIR_MED",
+            "MWIR_LOW", "HT_SINK_TEMP", "SWIR_TEMP", "CRC"
+        ]
+    }
+
     def __init__(self, log_file_name: str):
         """Initialise the iterator.
 
@@ -31,7 +43,7 @@ class EGSEDumpDecoder:
             raise Exception(f"Failed to open file: {e.strerror}")
 
     def __iter__(self):
-        """The iterator. 
+        """The iterator.
 
         This function reads the file, a line at a time and validates the
         CRC. If this are OK, the hex data is decoded using the parser in tm.py
@@ -44,7 +56,7 @@ class EGSEDumpDecoder:
                # Do something with timestamp and entry
 
         """
-         
+
         line_number = 0
         for line in self.in_file:
             line_number += 1
@@ -66,6 +78,7 @@ class EGSEDumpDecoder:
         self.in_file.seek(0)
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Decode data from egse log")
     parser.add_argument("-gnuplot", action="store_true", help="Output tab separated data and precede the header line with #")
     parser.add_argument("-fields", type=str, metavar="FIELD[,FIELD[,...]]", help="List of fields you want from the log")
@@ -96,25 +109,39 @@ if __name__ == "__main__":
     else:
         separator = ","
 
-    # Split up fields
-    if args.fields is None:
+    # If fields is specified and is "" then we use all fields.
+    # If it's specified and is a comma separated list, we use that list.
+    # If it's not specified then we look in default_fields_per_type. This
+    # latter option can only be done once we've started reading the file,
+    # since we don't know what type it is until that point.
+    if args.fields == "":
         args.fields = []
-    else:
+    elif args.fields is not None:
         args.fields = args.fields.split(",")
 
     try:
         printed_header = False
         for timestamp, entry in log_file:
+            if args.fields is None:
+                if type(entry) in log_file.default_fields_per_type:
+                    args.fields = log_file.default_fields_per_type[type(entry)]
+                else:
+                    args.fields = []
+
             # Print header line if not already printed.
             if not printed_header:
                 if args.gnuplot:
                     print("# ", end="", file=out_file)
-                print(f"Time", end=separator, file=out_file)
+                print(f"Date{separator}Time", end=separator, file=out_file)
                 print(entry.csv_header(*args.fields, separator=separator))
                 printed_header = True
 
             # Print data line.
-            print(timestamp, end=separator, file=out_file)
+            # To match pre-existing decodes, we need to split date/time
+            # into separate columns.
+            (date, timeofday) = timestamp.split(" ")
+            print(date, end=separator, file=out_file)
+            print(timeofday, end=separator, file=out_file)
             print(entry.csv(*args.fields, separator=separator), file=out_file)
     except Exception as e:
         print(f"Error: {str(e)}", file=sys.stderr)
