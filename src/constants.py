ccabd8cbb36468a4af1c911642463adad4c7fb1c
Giorgos <ucasgko@ucl.ac.uk>
2025-08-13 16:22:05 +0100

    [fix] : updated tm for errors with new hk struct

--- src/constants.py
+++ src/constants.py
@@ -10,12 +10,12 @@ CMD_SPEED_DICT = {
 }
 
 # ----Initialisation---------------------------------------------------------------------------------
-EXP_MODEL_ID = 0x07
+EXP_MODEL_ID = 0x02
 
 DEBUG_LEVEL = logging.INFO
 DEFAULT_PREFIX = datetime.now().strftime("%Y%m%dT%H%M%S")
 DEFAULT_PATH = Path.cwd() / "logs" / DEFAULT_PREFIX
-DEFAULT_COM_PORT = 20
+DEFAULT_COM_PORT = 4
 DEFAULT_CMD_SPEED = "Steady"  # "Steady" or "Fast"
 SCI_RESP_MARGIN = 0.020 # seconds
 
