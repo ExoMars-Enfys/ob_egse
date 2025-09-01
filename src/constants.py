eb03fe82b7bdacb0d4ff208519c186b39044c895
Alun Jones <alj118@aber.ac.uk>
2025-09-01 09:54:08 +0000

    Bring in constants.py from develop branch after checking laptop.

--- src/constants.py
+++ src/constants.py
@@ -5,19 +5,19 @@ from datetime import datetime
 from pathlib import Path
 
 CMD_SPEED_DICT = {
-    "Steady": 0.15,
-    "Fast": 0,
+    "Steady": 0.25,
+    "Fast": 0.05,
 }
 
 # ----Initialisation---------------------------------------------------------------------------------
-EXP_MODEL_ID = 0x02
+EXP_MODEL_ID = 0x07
 
 DEBUG_LEVEL = logging.INFO
 DEFAULT_PREFIX = datetime.now().strftime("%Y%m%dT%H%M%S")
 DEFAULT_PATH = Path.cwd() / "logs" / DEFAULT_PREFIX
-DEFAULT_COM_PORT = 4
-DEFAULT_CMD_SPEED = "Steady"  # "Steady" or "Fast"
-SCI_RESP_MARGIN = 0.020 # seconds
+DEFAULT_COM_PORT = 8
+DEFAULT_CMD_SPEED = "Fast"  # "Steady" or "Fast"
+SCI_RESP_MARGIN = 0.020  # seconds
 LOG_PREFIX = DEFAULT_PREFIX
 LOG_PATH = DEFAULT_PATH
 
