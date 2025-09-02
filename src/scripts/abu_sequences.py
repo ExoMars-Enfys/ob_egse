10d5661559980042de2018a9019ced14ff961286
Alun Jones <alj118@aber.ac.uk>
2025-09-02 08:14:29 +0000

    Add handling of high targets to find_dac_offset
    If the target is above 3900 then we'll use the medium gain value
    scaled by 30, as an estimate of the high gain output.

--- src/scripts/abu_sequences.py
+++ src/scripts/abu_sequences.py
@@ -489,6 +489,12 @@ def find_dac_offset(port: serial.rs485.RS485, sensor_name: str, target_output: i
     """
     event_log.info(f"Running abu targeted_binary_chop for {sensor_name} with target value {target_output}")
 
+    # If our target high gain output is above medium_gain_switch_threshold
+    # then we'll scale by medium_to_high_gain_scale and use the medium gain
+    # value instead. This gives a measure of protection against saturation.
+    medium_gain_switch_threshold = 3900
+    medium_to_high_gain_scale = 30
+
     if sensor_name not in ("MWIR", "SWIR"):
         event_log.error(f"For DAC offsets, sensor name must be either MWIR or SWIR, not {sensor_name}")
 
@@ -515,7 +521,10 @@ def find_dac_offset(port: serial.rs485.RS485, sensor_name: str, target_output: i
                 event_log.error(f"MWIR offset not updated in SCI. Got {sci.MWIR_OFFSET}, Expected: {test_value}")
 
             # Copy the reading so the rest of the loop doesn't depend on sensor.
-            reading = sci.MWIR_HIGH
+            if target_output <= medium_gain_switch_threshold:
+                reading = sci.MWIR_HIGH
+            else:
+                reading = medium_to_high_gain_scale*sci.MWIR_MED
         else:
             # Ditto for SWIR.
             tc.sci_offset(port, test_value, fixed_offset)
@@ -523,6 +532,11 @@ def find_dac_offset(port: serial.rs485.RS485, sensor_name: str, target_output: i
             if sci.SWIR_OFFSET != test_value:
                 event_log.error(f"SWIR offset not updated in SCI. Got {sci.SWIR_OFFSET}, Expected: {test_value}")
             reading = sci.SWIR_HIGH
+
+            if target_output <= medium_gain_switch_threshold:
+                reading = sci.SWIR_HIGH
+            else:
+                reading = medium_to_high_gain_scale*sci.SWIR_MED
 
         event_log.info(f"Got the following {sensor_name} high reading: {reading}")
 
