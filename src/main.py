f4eb1587e1cbc6bff7ffdc878a7f6065010df963
Alun Jones <alj118@aber.ac.uk>
2026-07-09 13:27:43 +0100

    Oops, forgot to comment out a couple of abu.* calls
    The only place main is now using abu.* is in calls to find_dac_offset.
    find_dac_offset does not use any other abu.* calls and does not perform
    any movements.

--- src/main.py
+++ src/main.py
@@ -399,10 +399,10 @@ def main() -> None:
         # ------------------------------------------------------------------------------------------
 
         # Ensure we're off either endstop when finishing up.
-        abu.move_off_endstops(ob_port)
+        #abu.move_off_endstops(ob_port)
 
         # Get final HK
-        abu.read_hk(ob_port)
+        #abu.read_hk(ob_port)
 
         # Auto-generate CSV files from HK and SCI logs
         # abu.convert_logs()
