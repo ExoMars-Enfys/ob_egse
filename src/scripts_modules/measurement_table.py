7d2118013ad35c7deae34fc638df23eaacbc8c82
EnfysEGSE <b.whiteside@ucl.ac.uk>
2026-07-10 14:25:26 +0100

    vscode decided to reformat.

--- src/scripts_modules/measurement_table.py
+++ src/scripts_modules/measurement_table.py
@@ -6,13 +6,14 @@ driving the tables backwards can give completely different results
 from driving them forwards. It's possible that the combination of dark
 and measurement tables could cause overlap, and it's not completely
 clear how this would be handled - would it cause backtracking, or is
-the combined table sorted in absolute position (or reverse absolute 
+the combined table sorted in absolute position (or reverse absolute
 position) order?
 
 The current implementation assumes that dark tables are iterated
 independently of the measurement tables, and so will generate backward
 movements when overlap happens.
 """
+
 
 class MeasurementTable:
     """An encapsulation of how the EB does measurement tables.
@@ -25,6 +26,7 @@ class MeasurementTable:
     be (non-recursively) pasted onto the front and back of the current table
     when scan is called.
     """
+
     LOW = 1000
     HIGH = 9960
 
@@ -52,7 +54,6 @@ class MeasurementTable:
                 table.append(abs_pos)
         return table
 
-
     def scan(self, start=None, end=None, start_motor_steps=LOW):
         """Run through the table, yielding resulting movements and positions.
 
@@ -78,7 +79,7 @@ class MeasurementTable:
             start = 0
 
         if end is None:
-            end = len(self.table)-1
+            end = len(self.table) - 1
 
         if end < 0 or end >= len(self.table) or start < 0 or start >= len(self.table):
             raise RuntimeError("Table start/end is out of range")
@@ -102,7 +103,7 @@ class MeasurementTable:
             step = 1
 
         while True:
-            yield (table[table_pos]-prev, table[table_pos])
+            yield (table[table_pos] - prev, table[table_pos])
             prev = table[table_pos]
             if table_pos == table_end:
                 break
@@ -120,7 +121,7 @@ class MeasurementTable:
         for pos in sorted(abs_positions):
             if pos > MeasurementTable.HIGH:
                 raise ValueError(f"Position {pos} is out of range")
-            rel_positions.append(pos-prev)
+            rel_positions.append(pos - prev)
             prev = pos
 
         return MeasurementTable(rel_positions)
@@ -128,38 +129,32 @@ class MeasurementTable:
     def __str__(self):
         return str(self.table)
 
+
 # Some example tables. The first two are the reserved dark "low" and
 # "high" end tables.
 predefined = [
     # Dark table 0: Fine resolution to capture the low edge in SWIR.
-    MeasurementTable.from_abs_position_list(range(1250, 1450, 5)).table,
-
+    MeasurementTable.from_abs_position_list(range(1250, 1451, 5)).table,
+    #
     # Dark table 1: Fine resolution to capture the high edge in SWIR,
     # along with a chunk at the end to capture SWIR BC.
     MeasurementTable.from_abs_position_list(
-        list(range(9100, 9301, 5)) +    # Edge of SWIR
-        list(range(9940, 9961, 2))      # SWIR BC
+        list(range(9100, 9301, 5))  # Edge of SWIR
+        + list(range(9940, 9961, 2))  # SWIR BC
     ).table,
-
+    #
     # Measurement table 2: End of DT0 thru start of DT1 in steps of
     # 30, with an extra bit around MWIR BC location.
-    MeasurementTable.from_abs_position_list(
-        list(range(1450, 9100, 30)) +
-        list(range(7990, 8011, 2))
-    ).table,
-
+    MeasurementTable.from_abs_position_list(list(range(1450, 9100, 30)) + list(range(7990, 8011, 2))).table,
+    #
     # Measurement table 3: End of DT0 thru start of DT1 in steps of
     # 5, with an extra bit around MWIR BC location.
-    MeasurementTable.from_abs_position_list(
-        list(range(1450, 9100, 5)) +
-        list(range(7990, 8011, 2))
-    ).table,
+    MeasurementTable.from_abs_position_list(list(range(1450, 9100, 5)) + list(range(7990, 8011, 2))).table,
 ]
 
-if __name__ == '__main__':
-    m = MeasurementTable(predefined[2],
-            before_table=MeasurementTable(predefined[0]), 
-            after_table=MeasurementTable(predefined[1])
+if __name__ == "__main__":
+    m = MeasurementTable(
+        predefined[2], before_table=MeasurementTable(predefined[0]), after_table=MeasurementTable(predefined[1])
     )
     for rel, abs_pos in m.scan():
         print(f"Relative {rel} steps, position is now {abs_pos}")
