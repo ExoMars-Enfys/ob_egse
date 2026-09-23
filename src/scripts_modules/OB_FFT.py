e1f22b6f4324dc4aada095843bcf8ed059d13eac
EnfysEGSE <b.whiteside@ucl.ac.uk>
2026-09-23 15:58:29 +0100

    [fix] :  putting back the dac offset algo

--- src/scripts_modules/OB_FFT.py
+++ src/scripts_modules/OB_FFT.py
@@ -663,7 +663,7 @@ def fft_stage_2(
     # region SCI ACQ with TEC
     response = checks.power(0x03, label="mechanism and detector boards power on")
     ui_runtime_controller.request_force_pause("Click to continue once ready for the science scan.")
-    # sci_acq.choose_dac_offsets(port, port_lock=port_lock, worker=worker)
+    sci_acq.choose_dac_offsets(port, port_lock=port_lock, worker=worker)
     capture_id = ui_runtime_controller.begin_ob_sci_capture("OB FFT Stage 2 TEC science scan")
     try:
         sci_acq.measurement_scan(port, step_spacing=50, port_lock=port_lock, worker=worker)
